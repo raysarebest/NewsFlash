db068411b4103d53de87d48b7b962b77a756a237
Michael Hulet <michael@hulet.tech>
2025-03-07 20:24:04 +0100

    Initial Commit

---
+++ NewsFlash/NewsFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFlashApp.swift
+//  NewsFlash
+//
+//  Created by Michael Hulet on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsFlashTests/NewsFlashTests.swift
@@ -0,0 +1,16 @@
+//
+//  NewsFlashTests.swift
+//  NewsFlashTests
+//
+//  Created by Michael Hulet on 3/7/25.
+//
+
+import Testing
+
+struct NewsFlashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
